4817c400e8776fdca8c765952cf110d7080746ab
tommitt <tommytassi@hotmail.it>
2025-03-02 18:18:52 +0100

    Organize repo as uv workspace

--- sdks/python/src/client.py
+++ sdks/python/src/client.py
@@ -1,13 +1,16 @@
 import os
 
 import httpx
-from eunomia.orchestra import Orchestra
 
 
 class EunomiaClient:
     def __init__(self, api_key: str = None, server_host: str = None) -> None:
-        self.api_key = api_key if api_key is not None else os.getenv("WAY_API_KEY", None)
-        self.server_host = server_host if server_host is not None else "http://localhost:8000"
+        self.api_key = (
+            api_key if api_key is not None else os.getenv("WAY_API_KEY", None)
+        )
+        self.server_host = (
+            server_host if server_host is not None else "http://localhost:8000"
+        )
 
         self.client = httpx.Client(
             base_url=self.server_host,
@@ -20,7 +23,7 @@ class EunomiaClient:
         response = self.client.get("/check_access/")
         response.raise_for_status()
         return response.json()
-    
+
     def allowed_resources(self, principal_id: str) -> list[str]:
         """Return the resources the principal specified by the principal_id has access to"""
         response = self.client.get("/allowed_resources/")
