d1e88b279a387b6f050a8a5fc60576bc70f84aab
vincenzopecorella <vincpeco@gmail.com>
2025-03-29 15:09:44 +0100

    RAG example + Fix docs

---
+++ examples/langchain_rag/example_langchain_rag.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+from eunomia_core import schemas
+from eunomia_sdk_langchain import EunomiaLoader
+from eunomia_sdk_langchain.retriever import EunomiaRetriever
+from eunomia_sdk_python import EunomiaClient
+from langchain_community.document_loaders import HuggingFaceDatasetLoader
+from langchain_community.retrievers import BM25Retriever
+
+client = EunomiaClient()
+
+## LOAD RESOURCES
+dataset_name = "Dev523/Crime-Reports-Dataset"
+page_content_column = "crimeaditionalinfo"
+loader = HuggingFaceDatasetLoader(dataset_name, page_content_column)
+eunomia_loader = EunomiaLoader(loader)
+docs = eunomia_loader.load()
+
+## DEFINE PRINCIPALS
+analyst_financial_fraud = client.register_entity(
+    type=schemas.EntityType.principal,
+    attributes={"role": "Analyst - Financial Fraud", "department": "Financial Fraud"},
+)
+
+analyst_media_fraud = client.register_entity(
+    type=schemas.EntityType.principal,
+    attributes={
+        "role": "Analyst - Media Related Fraud",
+        "department": "Media Related Crime",
+    },
+)
+
+director = client.register_entity(
+    type=schemas.EntityType.principal,
+    attributes={"role": "Director", "department": "Central"},
+)
+
+
+## DEFINE RETRIEVER
+bm25_retriever = BM25Retriever.from_documents(docs)
+
+analyst_financial_fraud_retriever = EunomiaRetriever(
+    retriever=bm25_retriever,
+    principal=schemas.PrincipalAccess(uri=analyst_financial_fraud.uri),
+)
+
+analyst_media_fraud_retriever = EunomiaRetriever(
+    retriever=bm25_retriever,
+    principal=schemas.PrincipalAccess(uri=analyst_media_fraud.uri),
+)
+
+director_retriever = EunomiaRetriever(
+    retriever=bm25_retriever,
+    principal=schemas.PrincipalAccess(uri=director.uri),
+)
+
+
+## RETRIEVE INFO WITH PERMISSIONS
+print(analyst_financial_fraud_retriever.invoke("unauthorized transaction"))
+print(analyst_media_fraud_retriever.invoke("facebook group"))
+print(analyst_media_fraud_retriever.invoke("robbery"))
--- pkgs/core/src/eunomia_core/schemas.py
+++ pkgs/core/src/eunomia_core/schemas.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from typing import Literal, Optional
 
-from pydantic import BaseModel, ConfigDict, Field, field_validator, model_validator
-
 from eunomia_core.enums import EntityType
 from eunomia_core.utils import generate_uri
+from pydantic import (BaseModel, ConfigDict, Field, field_validator,
+                      model_validator)
 
 
 class Attribute(BaseModel):
