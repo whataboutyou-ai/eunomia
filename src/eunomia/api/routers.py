d163fb9167d140b3c78d1a3fcabd7960abab37bf
tommitt <tommytassi@hotmail.it>
2025-07-03 16:32:15 +0200

    Refactor api

---
+++ src/eunomia/api/routers.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+
+from eunomia.engine.router import engine_router_factory
+from eunomia.fetchers.factory import FetcherFactory
+from eunomia.server import EunomiaServer
+from eunomia.server.router import server_router_factory
+
+
+def public_router_factory(server: EunomiaServer) -> APIRouter:
+    router = APIRouter()
+
+    router.include_router(server_router_factory(server), tags=["server"])
+
+    return router
+
+
+def admin_router_factory(server: EunomiaServer) -> APIRouter:
+    router = APIRouter()
+
+    router.include_router(engine_router_factory(server.engine), tags=["engine"])
+
+    for fetcher_id, fetcher_router in FetcherFactory.get_all_routers().items():
+        router.include_router(
+            fetcher_router, prefix=f"/fetchers/{fetcher_id}", tags=[fetcher_id]
+        )
+
+    return router
